a34733cb1f574043f149cad31234662cbb4f2eb2
Augusto-Zeni <augustozeni955@gmail.com>
2025-02-28 11:13:10 -0300

    fix: storybook deploy

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,16 +1,15 @@
 /// <reference types="vite/client" />
 
-import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({
-    // Adicione esta configuração
-    providerImportSource: '@mdx-js/react',
-    remarkPlugins: [],
-    rehypePlugins: [],
-  })],
+  plugins: [react()],
   base: '/learning-design-system/',
+  build: {
+    outDir: 'storybook-static',
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 1600,
+  },
 })
